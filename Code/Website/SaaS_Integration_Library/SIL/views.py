3a130b8f6628369a65de2c0b71ffd7180dda146f
Adam Merille <ameri012@fiu.edu>
2015-02-03 13:55:52 -0500

    Added basic template for index

--- Code/Website/SaaS_Integration_Library/SIL/views.py
+++ Code/Website/SaaS_Integration_Library/SIL/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("SaaS Integration Libary says hello!")
+	context = RequestContext(request)
+	
+	context_dict = {'mainmessage': "Explore Third Party APIs!"}
+	return render_to_response('SIL/index.html', context_dict, context)
--- Code/Website/SaaS_Integration_Library/SaaS_Integration_Library/settings.py
+++ Code/Website/SaaS_Integration_Library/SaaS_Integration_Library/settings.py
@@ -1,4 +1,11 @@
 # Django settings for SaaS_Integration_Library project.
+import os
+SETTINGS_DIR = os.path.dirname(__file__)
+
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -111,6 +118,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
