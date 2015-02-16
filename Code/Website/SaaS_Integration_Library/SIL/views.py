2e486f00e4d8ebd2c112b6108132654fcf6d3e27
Adam Merille <ameri012@fiu.edu>
2015-02-16 14:35:51 -0500

    fixed errors in base template rendering

--- Code/Website/SaaS_Integration_Library/SIL/views.py
+++ Code/Website/SaaS_Integration_Library/SIL/views.py
@@ -5,6 +5,5 @@ from SIL.models import Api_Credential
 
 def index(request):
 	context = RequestContext(request)
-	
 	context_dict = {'mainmessage': "Explore Third Party APIs!"}
 	return render_to_response('SIL/index.html', context_dict, context)
